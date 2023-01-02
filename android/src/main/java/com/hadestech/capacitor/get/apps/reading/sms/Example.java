92e071eb34bded196aa662d504506d5230444b3b
jiayee9797@hotmail.com <jiayee9797@hotmail.com>
2023-01-02 19:11:03 +0800

    echo

--- android/src/main/java/com/hadestech/capacitor/get/apps/reading/sms/Example.java
+++ android/src/main/java/com/hadestech/capacitor/get/apps/reading/sms/Example.java
@@ -1,11 +1,18 @@
 package com.hadestech.capacitor.get.apps.reading.sms;
 
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PermissionInfo;
+
+import java.util.ArrayList;
+import java.util.List;
 import android.util.Log;
 
 public class Example {
 
     public String echo(String value) {
         Log.i("Echo", value);
-        return value;
+        return "lalalalal";
     }
 }
