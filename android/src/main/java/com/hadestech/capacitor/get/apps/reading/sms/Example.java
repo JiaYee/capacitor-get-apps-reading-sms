fa890163f49613d32e4c6408b59e62d17b3e170a
jiayee9797@hotmail.com <jiayee9797@hotmail.com>
2023-01-02 21:23:36 +0800

    get list

--- android/src/main/java/com/hadestech/capacitor/get/apps/reading/sms/Example.java
+++ android/src/main/java/com/hadestech/capacitor/get/apps/reading/sms/Example.java
@@ -1,11 +1,16 @@
 package com.hadestech.capacitor.get.apps.reading.sms;
 
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.util.Log;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Example {
 
     public String echo(String value) {
         Log.i("Echo", value);
-        return "mamamia";
+        return "mamapapaya";
     }
 }
--- android/src/main/java/com/hadestech/capacitor/get/apps/reading/sms/ExamplePlugin.java
+++ android/src/main/java/com/hadestech/capacitor/get/apps/reading/sms/ExamplePlugin.java
@@ -1,10 +1,22 @@
 package com.hadestech.capacitor.get.apps.reading.sms;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.util.Log;
+import com.getcapacitor.JSArray;
 import com.getcapacitor.JSObject;
+import com.getcapacitor.JSObject;
+import com.getcapacitor.NativePlugin;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
+import com.getcapacitor.util.JSONUtils;
+import java.util.ArrayList;
+import java.util.List;
+import org.json.JSONObject;
 
 @CapacitorPlugin(name = "Example")
 public class ExamplePlugin extends Plugin {
@@ -13,10 +25,39 @@ public class ExamplePlugin extends Plugin {
 
     @PluginMethod
     public void echo(PluginCall call) {
-        String value = call.getString("value");
+        // Get the Context object
+        Context context = getContext();
 
-        JSObject ret = new JSObject();
-        ret.put("value", implementation.echo(value));
-        call.resolve(ret);
+        // Initialize the PackageManager
+        PackageManager pm = context.getPackageManager();
+        List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+
+        // Iterate through the list and print out the package names
+        List<Object> appsWithSmsPermission = new ArrayList<>();
+        for (ApplicationInfo packageInfo : packages) {
+            // Check if the app has the SMS permission
+            if (pm.checkPermission(Manifest.permission.READ_SMS, packageInfo.packageName) == PackageManager.PERMISSION_GRANTED) {
+                // If the app has the permission, add it to the list
+                if (packageInfo.sourceDir.startsWith("/data/app/")) {
+                    appsWithSmsPermission.add(pm.getApplicationLabel(packageInfo));
+                }
+            }
+        }
+
+        //        // Print out the list of apps with the SMS permission
+        //        for (Object app : appsWithSmsPermission) {
+        //            Log.println("app", app);
+        //        }
+
+        //        String value = call.getString("value");
+        JSObject retObj = new JSObject();
+        JSArray retList = new JSArray();
+        for (Object appObj : appsWithSmsPermission) {
+            retList.put(appObj);
+        }
+        //        ret2.put(appsWithSmsPermission);
+        //        ret.put("value", implementation.echo(value));
+        retObj.put("value", retList);
+        call.resolve(retObj);
     }
 }
